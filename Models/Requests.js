46eef3f77e12298c30efea7aa69240d1a1f9710b
Husayn Karimi <husaynkarimi@gmail.com>
2018-11-20 03:34:23 -0500

    added match function

--- Models/Requests.js
+++ Models/Requests.js
@@ -8,7 +8,8 @@ class Requests {
     const user_id = Users.getId(kerberos);
     const insert = `insert into request (user_id, type) values (${user_id}, '${type}');`;
     const response = await database.query(insert);
-    const request_id = response[0].id;
+    const sqlGetRequestID = `select id from request where user_id=${user_id} and type='${type}';`;
+    const request_id = sqlGetRequestId[0].id;
     intervals.forEach(async function(interval) {
       const startTime = date+interval[0]+":00";
       const endTime = date+interval[1]+":00";
@@ -48,6 +49,49 @@ class Requests {
   }
 
   static async match(requestId) {
-    const requstQuery = `select * from request where id=${requestId};`;
+    const requestQuery = `select * from request where id=${requestId};`;
+    const response = await database.query(requestQuery);
+    const user = response[0].user_id;
+    const type = response[0].type;
+    const intervalsReq = `select * from interval where request_id=${requestId};`;
+    const locationsReq = `select * from location where request_id=${requestId};`;
+
+    const allIntervals = `select * from interval where not(request_id=${requestId});`;
+
+    const matchFound = true;
+    let chosenDate = "";
+    const narrowedRequests = [];
+    intervalsReq.forEach(async function(intervalReq) {
+      intervalAlls.forEach(async function(interval) {
+        const reqStartTime = new Date(intervalReq.start_time);
+        const reqEndTime = new Date(intervalReq.end_time);
+        const intervalStartTime = new Date(interval.start_time);
+        const intervalEndTime = new Date(interval.end_time);
+        const intervalReqId = interval.request_id;
+        const sqlType = `select * from request where request_id=${intervalReqId} and not(type=${type});`;
+        const responseType = await database.query(sqlType);
+        if (reqStartTime <= intervalEndTime && reqStartTime >= intervalStartTime && responseType!=undefined) {
+          chosenDate = reqStartTime;
+          narrowedRequests.push(interval.request_id);
+        }
+        else if (reqEndTime <= intervalEndTime && reqEndTime >= intervalStartTime && responseType!=undefined) {
+          chosenDate = reqEndTime;
+          narrowedRequests.push(interval.request_id);
+        }
+        else {
+          // no overlap for interval
+        }
+      });
+    });
+
+    locationsReq.forEach(async function(location) {
+      narrowedRequests.forEach(async function(narrowedRequest) {
+        const sqlLocationsNarrowed = `select * from location where request_id=${narrowedRequest};`;
+        sqlLocationsNarrowed.forEach(async function(locationForNarrowed) {
+
+        });
+      });
+    });
+
   }
 }
