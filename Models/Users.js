87b3f63ef7874975ffb69ddf2a63b41e7b151e83
Husayn Karimi <husaynkarimi@gmail.com>
2018-11-19 20:12:17 -0500

    added functions to Users

---
+++ Models/Users.js
@@ -0,0 +1,47 @@
+const database = requiore('../database');
+
+class Users {
+  static async userExists(kerberos) {
+    const sql = `select * from user where kerberos =${kerberos}`
+    const response = await database.query(sql);
+    if (response[0]!==undefined) {
+      return true;
+    }
+    return false;
+  }
+
+  static async getId(kerberos) {
+    const sql = `select * from user where kerberos =${kerberos}`
+    const response = await database.query(sql);
+    if (response[0]!==undefined) {
+      return response[0].id;
+    }
+    return false;
+  }
+
+  static async getKerberos(id) {
+    const sql = `select * from users where id=${id}`
+    const response = await database.query(sql);
+    if (response[0]!==undefined) {
+      return response[0].kerberos;
+    }
+    return false;
+  }
+
+  static async getMatches(kerberos) {
+    const id = getId(kerberos);
+    if (id) {
+      const sql = `select * from users where host_id=${id} or guest_id=${id}`
+      const response = await database.query(sql);
+      if (response[0]) {
+        return response;
+      }
+      else {
+        return false;
+      }
+    }
+    return false;
+  }
+}
+
+module.exports = Users;
