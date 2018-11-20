6680a75dde5dc7dd89725fbaf07debe682a46e6d
vcvos52 <vcvos52@mit.edu>
2018-11-19 20:16:24 -0500

    iterating on routes

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,8 @@ const router = express.Router();
 
 const axios = require('axios');
 
+const Users = require('../Models/Users.js')
+
 
 /**
  * Authenticate
@@ -18,7 +20,14 @@ const axios = require('axios');
  * @throws {403} - if the username is from MIT
 */
 router.post('/:user', async (req, res) => {
-    let user = req.param.user;
+    let kerberos = req.param.user;
+    // if the kerberos does not exist, return that
+    if (!(await Users.userExists(kerberos))){
+        res.status(403).json("This is not a valid user.").end();
+        return;
+    }
+    req.session.name = kerberos;
+    res.status(201).json(newUser).end();
 
 });
 
@@ -31,9 +40,45 @@ router.post('/:user', async (req, res) => {
  * @throws {404} - No matches found
 */
 router.get('/matches/:user', async (req, res) => {
-    let user = req.param.user;
+    let kerberos = req.param.user;
+    // if the kerberos does not exist, return that
+    if (!(await Users.userExists(kerberos))){
+        res.status(403).json("This is not a valid user.").end();
+        return;
+    }
+    let matches = await Users.getMatches(kerberos);
+    if (!(matches)){
+        res.status(404).json("This user is not matched.").end();
+        return;
+    }
+    let matchData = shapeDate(matches, req);
+    res.status(201).json(matchData).end();
 
 });
+
+
+/**
+ * Takes in the data, and reformats it to be more readable in the view
+ * @param matches {List<JSON>} - [{ID: , Guest: , Host: , Time: , Location: }, ...]
+ * @returns {List<JSON>} - [{role: , otherPerson: , Time: , Location: }, ...]
+ */
+function shapeDate(matches, req){
+    for (let i = 0; i < matches.length; i++){
+        let row = matches[i];
+        let newRow = {};
+        // need to make sure the column names are correct
+        if (row["Guest"] === req.session.name){
+            newRow["role"] = "guest";
+            newRow["otherPerson"] = row["Host"];
+        } else if (row["Host"] === req.session.name){
+            newRow["role"] = "host";
+            newRow["otherPerson"] = row["Guest"];
+        }
+        newRow["Time"] = row["Time"];
+        newRow["Location"] = row["Location"];
+        return newRow;
+    }
+}
 
 
 module.exports = router;
