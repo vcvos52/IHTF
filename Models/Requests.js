dd3458d8c1b76b4dcfa52cc47fb4410463b58eed
Husayn Karimi <husaynkarimi@gmail.com>
2018-11-20 01:18:19 -0500

    added datetime

--- Models/Requests.js
+++ Models/Requests.js
@@ -5,29 +5,23 @@ const Users = require('../Users');
 class Requests {
 
 
-  static async addRequest(type, kerberos, locations, intervals) {
+  static async addRequest(type, kerberos, locations, datetime, intervals) {
+    // TODO: check if there is outstanding request by same user at same time
     const user_id = Users.getId(kerberos);
-    if (requestExists(kerberos, type)) {
-      // outstanding request of same type already exists
-      return false;
-    }
-    // request does not exist already
-    else {
-      const insert = `insert into request (user_id, type) values (${user_id}, '${type}');`;
-      const response = await database.query(insert);
-      const request_id = response[0].id;
-      intervals.forEach(function(interval) {
-        const startTime = interval[0];
-        const endTime = interval[1];
-        const insertInterval = `insert into interval (request_id, start_time, end_time) values (${request_id}, ${startTime}, ${endTime});`;
-        await database.query(insertInterval);
-      });
-      locations.forEach(function(location) {
-        const dining_hall_id = getLocationId(location);
-        const insertLocation = `insert into location (request_id, dining_hall_id), values (${request_id}, ${dining_hall_id});`;
-        await database.query(insertLocation);
-      });
-    }
+    const insert = `insert into request (user_id, type) values (${user_id}, '${type}');`;
+    const response = await database.query(insert);
+    const request_id = response[0].id;
+    intervals.forEach(function(interval) {
+      const startTime = interval[0];
+      const endTime = interval[1];
+      const insertInterval = `insert into interval (request_id, start_time, end_time) values (${request_id}, ${startTime}, ${endTime});`;
+      await database.query(insertInterval);
+    });
+    locations.forEach(function(location) {
+      const dining_hall_id = getLocationId(location);
+      const insertLocation = `insert into location (request_id, dining_hall_id), values (${request_id}, ${dining_hall_id});`;
+      await database.query(insertLocation);
+    });
   }
 
   static async requestExists(kerberos, type) {
