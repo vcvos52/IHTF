5fed656d9ce594b6bd58e799312224deaf92784a
Giulio Capolino <>
2018-11-19 19:34:50 -0500

    Initialized vue components

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,101 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <h2> Welcome to Have This Food!</h2>
     </div>
-    <router-view/>
+    <div v-if="logged === false">
+           <Login/>
+    </div>
+    <div v-else-if="logged === true">
+           
+    </div>
+    <div id="copyrights">
+      Made by Surf the High C's
+    </div>
   </div>
 </template>
 
+
+<script>
+import { eventBus } from "./main";
+import Login from "./components/Login";
+export default {
+  name: "app",
+
+  props: {
+    logged: Boolean
+  },
+
+  components: {
+    Login
+    // Meals,
+    // Choice,
+    // Donate,
+    // Receive
+  },
+
+  data() {
+    return {
+      currentAction: "choice",
+      logged: false
+    };
+  },
+
+  // adds the reactive aspect to the app. Every time the currentAction changes,
+  // the app will change (read by this eventBus)
+  created: function() {
+    eventBus.$on("update-action", res => {
+      this.currentAction = res;
+    });
+    eventBus.$on("login-action", () => {
+      this.logged = true;
+    });
+    eventBus.$on("logout-action", () => {
+      this.logged = false;
+    });
+  }
+};
+</script>
+
+
 <style>
+* {
+  background-color: #ffebaf;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Pacifico", cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  margin: 0;
+}
+
+#nav h2 {
+  color: white;
+  background-color: #f1b101;
+  margin: 0;
+  padding: 10px;
   text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+#copyrights {
+  font-family: "Pacifico", sans-serif;
+  font-size: 70%;
+  text-align: center;
+  padding-bottom: 5px;
+  border-top: 1px solid black;
+  width: 100%;
+  position: fixed;
+  bottom: 0;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.button {
+  margin-top: 10px;
+}
+
+input {
+  outline: 0;
+  margin-left: 10px;
+  background-color: white;
 }
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id=login>
+        <form class="component" v-on:submit.prevent='login' method="post">
+            <h4> Login with your Kerberos: </h4>
+            
+            <div class="short-answer">
+                <label for="kerberos"> Kerberos: </label>
+                <input id="kerberos" v-model="kerboras" type="text" name="kerberos">
+            </div>
+            
+            <input type="submit" value="Login" class="button">
+
+            <div v-if="error" class="error-message">
+                <b> {{error}} </b>
+            </div>
+        </form>
+    </div>
+</template>
+
+
+<script>
+import { eventBus } from "../main";
+export default {};
+</script>
+
+
+<style scoped>
+#login {
+  padding-top: 40px;
+  width: 100%;
+  position: fixed;
+  font-family: "EB Garamond", serif;
+  text-align: center;
+}
+</style>
