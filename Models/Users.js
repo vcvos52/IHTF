e401b8519a8d2fca1e28feec7e6c4cbe5b1b3c0e
Giulio Capolino <>
2018-12-03 03:38:30 -0500

    Added feature for Pending Request. Set up front end for deleting request and meals. Fixed bug on why name for Daniel was not appearing

--- Models/Users.js
+++ Models/Users.js
@@ -1,15 +1,15 @@
 const database = require('../database');
 
 class Users {
- /**
-  * checks whether kerberos 'kerberos' exists in user table
-  * @param  {string}  kerberos kerberos string to check
-  * @return {boolean} true if kerberos exists in user table, false if not
-  */
+  /**
+   * checks whether kerberos 'kerberos' exists in user table
+   * @param  {string}  kerberos kerberos string to check
+   * @return {boolean} true if kerberos exists in user table, false if not
+   */
   static async userExists(kerberos) {
     const sql = `select * from user where kerberos='${kerberos}';`;
     const response = await database.query(sql);
-    if (response[0]!==undefined) {
+    if (response[0] !== undefined) {
       return true;
     }
     return false;
@@ -23,7 +23,7 @@ class Users {
   static async getId(kerberos) {
     const sql = `select * from user where kerberos='${kerberos}';`;
     const response = await database.query(sql);
-    if (response[0]!==undefined) {
+    if (response[0] !== undefined) {
       return response[0].id;
     }
     return false;
@@ -37,7 +37,7 @@ class Users {
   static async getKerberos(id) {
     const sql = `select * from user where id=${id}`
     const response = await database.query(sql);
-    if (response[0]!==undefined) {
+    if (response[0] !== undefined) {
       return response[0].kerberos;
     }
     return false;
@@ -53,7 +53,7 @@ class Users {
     if (id) {
       const sql = `select * from meal where host_id=${id} or guest_id=${id}`
       const response = await database.query(sql);
-      if (response[0]) {
+      if (response[0] !== undefined) {
         return response;
       }
       else {
@@ -61,6 +61,43 @@ class Users {
       }
     }
     return false;
+  }
+
+  /**
+   * returns all requests made by kerberos 'kerberos'
+   * @param {string} kerberos string to query for
+   * @return {object} entries of requests or false if kerberos does not exist or no requests
+   * where request contains request id, start time, end time, and dining hall id
+   */
+  static async getRequests(kerberos) {
+    const id = await Users.getId(kerberos);
+    if (id) {
+      const sql = `SELECT i.request_id, i.start_time, i.end_time, location.dining_hall_id 
+        FROM \`interval\` as i INNER JOIN location on i.request_id = location.request_id 
+        WHERE i.request_id in (SELECT id FROM request WHERE user_id = ${id}) ORDER BY i.request_id`
+      const response = await database.query(sql);
+      if (response[0]) {
+        return response;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * returns 1 if request is host request, 2 if it's guest request, false if id does not exist
+   * @param {int} id - request id
+   * @return 1 or 2, false
+   */
+  static async getRequestType(id) {
+    const sql = `SELECT type FROM request WHERE id = ${id}`
+    const response = await database.query(sql);
+    if (response[0] !== undefined) {
+      if (response[0].type == 'host') {
+        return 1
+      } else if (response[0].type == 'guest') {
+        return 2
+      }
+    }
   }
 }
 
